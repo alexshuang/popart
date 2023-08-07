c928a8744a65e3ff92fe54d1bf840f2444902c28
Shuang Huang <alexhu@graphcore.ai>
2023-08-07 09:11:10 +0000

    update metagraph2onnx_bili_with_unpacked_inputs.sh for unique inputs

--- scripts/metagraph2onnx_bili_with_unpacked_inputs.sh
+++ scripts/metagraph2onnx_bili_with_unpacked_inputs.sh
@@ -6,11 +6,15 @@ set -ex
 
 grep 'Recv:' $1 | awk -F' ' '{print $2}' > inputs.txt
 sed -i 's/^"\(.*\)"$/\1,/g' inputs.txt
+sort inputs.txt | uniq > /tmp/inputs.txt
+cp /tmp/inputs.txt inputs.txt
 sed -i ':a;N;$!ba;s/\n//g' inputs.txt
 sed -i 's/,$//g' inputs.txt
 
 grep -r 'score\>' $1 | grep -v 'input: ' | awk -F' ' '{print $2}' > outputs.txt
 sed -i 's/^"\(.*\)"$/\1,/g' outputs.txt
+sort outputs.txt | uniq > /tmp/outputs.txt
+cp /tmp/outputs.txt outputs.txt
 sed -i ':a;N;$!ba;s/\n//g' outputs.txt
 sed -i 's/,$//g' outputs.txt
 
