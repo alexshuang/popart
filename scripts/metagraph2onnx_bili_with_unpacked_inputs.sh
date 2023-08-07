0ac4ba8e0345dcf697f6ea265c9c5e7f219d39ff
Shuang Huang <alexhu@graphcore.ai>
2023-08-07 01:22:29 +0000

    add metagraph2onnx_bili_with_unpacked_inputs.sh for bilibili models

---
+++ scripts/metagraph2onnx_bili_with_unpacked_inputs.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+set -ex
+
+# $1: metagraph.txt $2: metagraph
+
+grep 'Recv:' $1 | awk -F' ' '{print $2}' > inputs.txt
+sed -i 's/^"\(.*\)"$/\1,/g' inputs.txt
+sed -i ':a;N;$!ba;s/\n//g' inputs.txt
+sed -i 's/,$//g' inputs.txt
+
+grep -r 'score\>' $1 | grep -v 'input: ' | awk -F' ' '{print $2}' > outputs.txt
+sed -i 's/^"\(.*\)"$/\1,/g' outputs.txt
+sed -i ':a;N;$!ba;s/\n//g' outputs.txt
+sed -i 's/,$//g' outputs.txt
+
+INPUTS=`cat inputs.txt`
+OUTPUTS=`cat outputs.txt`
+
+python -m tf2onnx.convert --checkpoint $2 --output model.onnx --inputs $INPUTS --outputs $OUTPUTS --opset 11 --custom-ops TensorswitchStreamRecvInit,TensorswitchStreamRecv
